c2c2f8369487dfe1f7b0c46eb0afff31b12e2490
Ahmed Elshami <ahmed.n.alshamy@gmail.com>
2024-08-12 13:13:25 +0300

    first commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import style from './Home.module.css'
+
+import MainSlider from '../MainSlider/MainSlider'
+import CategorySlider from '../CategorySlider/CategorySlider'
+import FeaturedProducts from '../FeaturedProducts/FeaturedProducts'
+
+
+export default function Home() {
+  return (
+   <>
+    <div className={style.mainslider}>
+      <MainSlider/>
+    </div>
+    <CategorySlider/>
+    <FeaturedProducts/>
+   </>
+  )
+}
